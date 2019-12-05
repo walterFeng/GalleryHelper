d58a034faea5acad1d1fbba74cf8d390950b734d
walter <xiao.feng@yourdream.cc>
2019-12-05 20:23:07 +0800

    feat: add image page to demo

--- app/src/main/java/com/walter/gallery/MainActivity.kt
+++ app/src/main/java/com/walter/gallery/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.walter.gallery
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 /**
  * Created by walter on 2019/12/4.
@@ -20,71 +19,84 @@ import android.widget.TextView
  */
 class MainActivity : AppCompatActivity() {
 
+    private var orientation = LinearLayoutManager.HORIZONTAL
+
+    private var helper: GalleryHelper? = null
+
+    private val images = arrayListOf(
+        R.drawable.wechat_img137,
+        R.drawable.wechat_img138,
+        R.drawable.wechat_img139,
+        R.drawable.wechat_img140
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        init()
+    }
+
+    private fun init() {
         // init recyclerView:
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val adapter = setupAdapter(recyclerView)
 
         // support gallery:
-        val helper = GalleryHelper.from(recyclerView)
+        helper = GalleryHelper.from(recyclerView)
             .attach(loopParams = 500, itemSpace = 40, scale = 0.87f, alpha = 0.9f)
 
-        // add page changed listener
-        helper.pagingScrollHelper
-            .setOnPageChangedListener(object : PagingScrollHelper.OnPageChangedListener {
-                override fun onPageChange(index: Int) {
-                    Log.d("walter", "onPageChange:$index")
-                    val i = helper.lopperHelper.gerCurrentRealPosition()
-                    val position = i % adapter.itemCount
-                    Log.d("walter", "position=$position")
-                }
-            })
+        // add page changed listener:
+        helper?.pagingScrollHelper?.setOnPageChangedListener(object :
+            PagingScrollHelper.OnPageChangedListener {
+            override fun onPageChange(index: Int) {
+                val i = helper?.lopperHelper?.gerCurrentRealPosition() ?: 0
+                val position = i % adapter.itemCount
+                Log.d("walter", "onPageChange:$index || position=$position")
+            }
+        })
     }
 
     private fun setupAdapter(recyclerView: RecyclerView): RecyclerView.Adapter<RecyclerView.ViewHolder> {
-        recyclerView.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.HORIZONTAL, //has support LinearLayoutManager.VERTICAL 支持垂直滑动
-            false
-        )
+        recyclerView.layoutManager = LinearLayoutManager(this, orientation, false)
         recyclerView.adapter = object : RecyclerView.Adapter<ViewHolder>() {
             override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-                return ViewHolder(TextView(this@MainActivity))
+                return ViewHolder(ImageView(this@MainActivity))
             }
 
             override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
                 viewHolder.onBind(i)
             }
 
-            override fun getItemCount(): Int {
-                return 3
-            }
+            override fun getItemCount(): Int = images.size
         }
         return recyclerView.adapter!!
     }
 
+    @Suppress("all")
+    fun changeOrientation(v: View) {
+        orientation = if (orientation == LinearLayoutManager.HORIZONTAL)
+            LinearLayoutManager.VERTICAL else LinearLayoutManager.HORIZONTAL
+        helper?.detached()
+        init()
+    }
 
     internal inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private var textView: TextView = itemView as TextView
+        private var imageView: ImageView = itemView as ImageView
 
         init {
-            textView.textSize = 28f
             val params = ViewGroup.MarginLayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
-            textView.layoutParams = params
-            textView.gravity = Gravity.CENTER
-            textView.setBackgroundColor(Color.RED)
+            imageView.layoutParams = params
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setBackgroundColor(Color.RED)
         }
 
-        @SuppressLint("SetTextI18n")
         fun onBind(position: Int) {
-            textView.text = if (position == 0) "I am \n the first image" else "image $position"
+            Glide.with(itemView.context).load(images[position]).into(imageView)
         }
     }
 }
